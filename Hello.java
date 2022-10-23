2fe897271417a76f602c99d8e4b192dac8a855dd
Arpan46 <113567793+Arpan46@users.noreply.github.com>
2022-10-23 22:53:06 +0530

    Update Hello.java
    Origin

--- Hello.java
+++ Hello.java
@@ -2,11 +2,7 @@ class Hello
 {
 Public static void main(String args[])
 {
-<<<<<<< HEAD
-System.out.println("hello");
-=======
-System.out.println(“hello from feature-101”);
->>>>>>> feature-101
+System.out.println("hello from origin");
 }
 }
 
