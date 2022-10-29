8a1f431b922bc27bcfb6df16b71efe7e79f41e55
“Arpan” <arpanman20@gmail.com>
2022-10-29 14:48:38 +0000

    modified

--- Hello.java
+++ Hello.java
@@ -2,11 +2,8 @@ class Hello
 {
 Public static void main(String args[])
 {
-<<<<<<< HEAD
-System.out.println("hello from master ");
-=======
 System.out.println("hello from origin");
->>>>>>> 2fe897271417a76f602c99d8e4b192dac8a855dd
+
 }
 }
 
